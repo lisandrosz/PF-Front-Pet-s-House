dd8da50405e80db6b2bfe97422c70a194cf5062a
lisandrosz <szyzuklisandro@gmail.com>
2023-02-16 19:12:15 -0300

    components

---
+++ src/Componentes/SearchBar/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SearchBar: React.FC = () => {
+  return (
+    <div>
+      SearchBar
+      <input type="text" />
+      <button>Buscar</button>
+    </div>
+  );
+};
+
+export default SearchBar;
