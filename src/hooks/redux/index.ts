fb8e4f6c4704223f3c0416aec2f5e4b034ff7418
lisandrosz <szyzuklisandro@gmail.com>
2023-02-16 15:23:40 -0300

    fixed

--- src/hooks/redux/index.ts
+++ src/hooks/redux/index.ts
@@ -3,5 +3,5 @@ import type { TypedUseSelectorHook } from 'react-redux';
 import type { RootState, Dispatch } from 'redux/store';
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
-export const useAppDispatch: () => Dispatch = useDispatch;
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useCustomDispatch: () => Dispatch = useDispatch;
+export const useCustomSelector: TypedUseSelectorHook<RootState> = useSelector;
