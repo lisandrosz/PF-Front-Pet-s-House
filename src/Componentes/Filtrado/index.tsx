20c90d619d77a5688412cd977983ae0ab4bb5792
lisandrosz <szyzuklisandro@gmail.com>
2023-02-17 11:48:36 -0300

    changeHandler agregado

--- src/Componentes/Filtrado/index.tsx
+++ src/Componentes/Filtrado/index.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import type { ChangeEvent } from 'react';
 
 const Filtrado: React.FC = () => {
+  const changeHandler = (ev: ChangeEvent<HTMLSelectElement>): any => {
+    const name: string = ev.target.name;
+    const value: string = ev.target.value;
+    console.log(name, value);
+  };
+
   return (
     <>
       Filtrado
       <br />
       <label htmlFor="especie">Selecciones una especie</label>
-      <select name="especie" id="1">
+      <select name="especie" id="1" onChange={changeHandler}>
         <option value="todos">Todas las mascotas</option>
         <option value="perros">Perros</option>
         <option value="gatos">Gatos</option>
@@ -16,7 +23,7 @@ const Filtrado: React.FC = () => {
       <br />
       <br />
       <label htmlFor="tamaño">Selecciones el tamaño</label>
-      <select name="tamaño" id="2">
+      <select name="tamaño" id="2" onChange={changeHandler}>
         <option value="todos">Todas los tamaños</option>
         <option value="grande">Grande</option>
         <option value="mediano">Mediano</option>
@@ -25,7 +32,7 @@ const Filtrado: React.FC = () => {
       <br />
       <br />
       <label htmlFor="provincia">Selecciones la ubicacion</label>
-      <select name="provincia" id="3">
+      <select name="provincia" id="3" onChange={changeHandler}>
         <option value="todas">Todas las provincias</option>
         <option value="buenosaires">Buenos Aires</option>
         <option value="cordoba">Cordoba</option>
@@ -36,7 +43,7 @@ const Filtrado: React.FC = () => {
       <br />
       <p>Ordenado</p>
       <label htmlFor="edad">Ordenar por edad</label>
-      <select name="edad" id="4">
+      <select name="edad" id="4" onChange={changeHandler}>
         <option value="defecto">Por defecto</option>
         <option value="menor-mayor">De menor a mayor</option>
         <option value="mayor-menor">De mayor a menor</option>
@@ -44,7 +51,7 @@ const Filtrado: React.FC = () => {
       <br />
       <br />
       <label htmlFor="tiempo">Ordenar por tiempo publicado</label>
-      <select name="tiempo" id="5">
+      <select name="tiempo" id="5" onChange={changeHandler}>
         <option value="defecto">Por defecto</option>
         <option value="nuevo">Mas nuevas primero</option>
         <option value="antiguas">Mas antiguas primero</option>
