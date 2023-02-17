a9ab66b7e7a7ebcabc08a8e58520309bb15d9247
Seba20078 <seba20078@gmail.com>
2023-02-17 00:01:17 -0300

    Primer codigo del Nav y Search Bar

--- src/Componentes/SearchBar/index.tsx
+++ src/Componentes/SearchBar/index.tsx
@@ -1,11 +1,39 @@
-import React from 'react';
+// import React from 'react';
+// import { useCustomDispatch } from 'hooks/redux';
+import React, { useState } from 'react';
 
 const SearchBar: React.FC = () => {
+  const [name, setName] = useState('');
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>): void {
+    console.log(name);
+    setName(e.target.value);
+  }
+
+  const handleSubmit = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ): void => {
+    e.preventDefault();
+    // useCustomDispatch(name);
+    setName('');
+  };
   return (
     <div>
       SearchBar
-      <input type="text" />
-      <button>Buscar</button>
+      <input
+        type="text"
+        placeholder="Buscar..."
+        onChange={(e) => {
+          handleChange(e);
+        }}
+      />
+      <button
+        type="submit"
+        onClick={(e) => {
+          handleSubmit(e);
+        }}
+      >
+        Buscar
+      </button>
     </div>
   );
 };
