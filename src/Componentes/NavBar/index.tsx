968894ba2922dcd1fad1d135fb48fe3e18b945ce
unknown <candelaria.dipinto@gmail.com>
2023-02-17 16:18:12 -0300

    componentes añadidos

--- src/Componentes/NavBar/index.tsx
+++ src/Componentes/NavBar/index.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import SearchBar from 'Componentes/SearchBar';
-import { Link } from 'react-router-dom';
 
 const NavBar: React.FC = () => {
   return (
     <>
       <div>
         NavBar
-        <Link to=""></Link>
         <SearchBar />
       </div>
     </>
