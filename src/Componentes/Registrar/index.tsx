2388aa03818fb88ea1bcc10e37b6b7bbd5733e50
Seba20078 <seba20078@gmail.com>
2023-02-22 17:42:52 -0300

    empezndo register

--- src/Componentes/Registrar/index.tsx
+++ src/Componentes/Registrar/index.tsx
@@ -1,14 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Registrar: React.FC = () => {
+    const interface User = {
+kkk,
+    }
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [image, setImage] = useState('');
+
+  const [errors, setErrors] = useState({
+    empty: true
+  });
+  function handleChangeName(e: React.ChangeEvent<HTMLInputElement>): void {
+    setName(e.target.value);
+    console.log(name);
+  }
+  function handleChangeEmail(e: React.ChangeEvent<HTMLInputElement>): void {
+    setEmail(e.target.value);
+    console.log(email);
+  }
+  function handleChangePassword(e: React.ChangeEvent<HTMLInputElement>): void {
+    setPassword(e.target.value);
+    console.log(password);
+  }
+  function handleChangeImage(e: React.ChangeEvent<HTMLInputElement>): void {
+    setImage(e.target.value);
+    console.log(image);
+  }
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>): void {
+    e.preventDefault();
+    setRegistro(registro);
+  }
   return (
     <div>
       <h1>Registrarse</h1>
       <h3>Por favor, completa tus datos</h3>
-      <form>
+      <form
+        onSubmit={(e) => {
+          handleSubmit(e);
+        }}
+      >
         <div>
           <label htmlFor="fullname">Nombre completo</label>
-          <input name="fullname" placeholder="Nombre y Apellido" />
+          <input
+            name="fullname"
+            placeholder="Nombre y Apellido"
+            onChange={(e) => {
+              handleChangeName(e);
+            }}
+          />
         </div>
         <div>
           <label htmlFor="email">Correo electronico</label>
