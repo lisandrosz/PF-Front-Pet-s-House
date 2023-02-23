7c6c1cfebc57546131017fb76b4956ee2cb25fcf
unknown <candelaria.dipinto@gmail.com>
2023-02-23 12:34:10 -0300

    entendiendo mui

--- src/Componentes/NavBar/index.tsx
+++ src/Componentes/NavBar/index.tsx
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 import { setHome } from 'redux/slices/mascotas';
 import { useCustomDispatch } from 'hooks/redux';
 import './styleNavbar.css';
+import Button from '@mui/material/Button';
+// import { Home } from '@mui/icons-material';
 
 const NavBar: React.FC = () => {
   const dispatch = useCustomDispatch();
@@ -14,19 +16,19 @@ const NavBar: React.FC = () => {
   return (
     <div className="navbar">
       <Link to={'/'} style={{ textDecoration: 'none' }} onClick={handleSubmit}>
-        <button>Home</button>
+        <Button>Home</Button>
       </Link>
       <Link to={'/donacion'} style={{ textDecoration: 'none' }}>
-        <button>Donacion</button>
+        <Button>Donacion</Button>
       </Link>
       <Link to={'/favoritos'} style={{ textDecoration: 'none' }}>
-        <button>Favoritos</button>
+        <Button>Favoritos</Button>
       </Link>
       <Link to={'/publicar'} style={{ textDecoration: 'none' }}>
-        <button>Publicar Mascota</button>
+        <Button>Publicar Mascota</Button>
       </Link>
       <Link to={'/misPublicaciones'} style={{ textDecoration: 'none' }}>
-        <button>Mis Publicaciones</button>
+        <Button>Mis Publicaciones</Button>
       </Link>
 
       <SearchBar />
