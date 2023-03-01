fa835a0b334b7b49f7fda27f73850bcf5686d9da
lisandrosz <szyzuklisandro@gmail.com>
2023-03-01 19:42:07 -0300

    paginado funcionando

--- src/Componentes/Home/Home.tsx
+++ src/Componentes/Home/Home.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
-import CardsContainer from 'Componentes/CardsContainer';
 import Filtrado from 'Componentes/Filtrado';
+import Paginado from 'Componentes/Paginado';
 import './Home.css';
 import { traerPets } from 'helpers';
 import { useNavigate } from 'react-router-dom';
@@ -20,7 +20,7 @@ const Home: React.FC = () => {
         </div>
         <div className="cartas">
           <hr />
-          <CardsContainer />
+          <Paginado />
         </div>
       </div>
       <button
---
+++ src/Componentes/Paginado/index.tsx
@@ -0,0 +1,35 @@
+import React, { type ChangeEvent, useState } from 'react';
+import { Pagination } from '@mui/material';
+import usePagination from 'hooks/Paginacion';
+import { useCustomSelector } from 'hooks/redux';
+import CardsContainer from 'Componentes/CardsContainer';
+
+const Paginado: React.FC = () => {
+  const data = useCustomSelector((state) => state.pets.pets);
+  const [page, setPage] = useState(1);
+  const porPagina = 3;
+
+  const count = Math.ceil(data.length / porPagina);
+  const pets = usePagination(data, porPagina);
+
+  const handleChange = (event: ChangeEvent<unknown>, page: number): void => {
+    setPage(page);
+    pets.jump(page);
+  };
+
+  return (
+    <div>
+      <CardsContainer pets={pets.currentData()} />
+      <Pagination
+        count={count}
+        size="large"
+        page={page}
+        variant="outlined"
+        shape="rounded"
+        onChange={handleChange}
+      />
+    </div>
+  );
+};
+
+export default Paginado;
