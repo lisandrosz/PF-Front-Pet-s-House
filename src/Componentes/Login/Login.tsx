157b7a2e3397d6c36849a294f2d765143e52a81d
Kevin <kevinesteban2010@hotmail.com>
2023-03-03 12:22:43 -0500

    fix host

--- src/Componentes/Login/Login.tsx
+++ src/Componentes/Login/Login.tsx
@@ -43,7 +43,7 @@ const Login: React.FC = () => {
     e.preventDefault();
     try {
       await axios
-        .get<Pet[]>(`http://localhost:3001/users/login/${email}/${password}`)
+        .get<Pet[]>(`/users/login/${email}/${password}`)
         .then((res: { data: any }) => {
           if (typeof res.data === 'string') {
             Swal.fire({
