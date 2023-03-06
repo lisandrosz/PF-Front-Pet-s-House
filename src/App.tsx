72baefac1a4f7fabd41a067b3d2d9f4b20dbd66c
Seba20078 <seba20078@gmail.com>
2023-03-06 19:08:50 -0300

    Mi perfil actualizado

--- src/App.tsx
+++ src/App.tsx
@@ -40,7 +40,7 @@ const App: React.FC = () => {
         <Route path="/cambiarContraseña" element={<CambioContraseña />} />
         <Route path="/success" element={<Success />} />
         <Route path="/failure" element={<Failure />} />
-        <Route path="/usuario" element={<UserDetail />} />
+        <Route path="/miPerfil" element={<UserDetail />} />
       </Routes>
     </Provider>
   );
