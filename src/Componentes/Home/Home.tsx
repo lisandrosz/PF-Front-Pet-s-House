33767d03fd7530aca41d4e34151f2f6b054658c2
lucasibar <lucas.ibar.soy.henry@gmail.com>
2023-03-08 01:25:21 -0300

    DASHBORD USUARIO + login en navBar

--- src/Componentes/Home/Home.tsx
+++ src/Componentes/Home/Home.tsx
@@ -4,42 +4,22 @@ import Paginado from 'Componentes/Paginado';
 import './Home.css';
 import { traerPets } from 'helpers';
 import { useAuth0 } from '@auth0/auth0-react';
-import Swal from 'sweetalert2';
-import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { getLogged } from '../../helpers';
-import { useCustomSelector } from 'hooks/redux';
 
 const Home: React.FC = () => {
-  const { isAuthenticated, user, logout, loginWithRedirect } = useAuth0();
-  const logged = useCustomSelector((state) => state.users.logged);
-  const navigate = useNavigate();
+  const { isAuthenticated, user } = useAuth0();
   useEffect((): void => {
     traerPets();
-  });
+    const id = localStorage.getItem('id');
+    if (id !== null) getLogged(true);
+  }, []);
   useEffect((): void => {
     if (Boolean(isAuthenticated) && user != null) {
-      getLogged(true);
       const { name, email, image } = user;
       auth0Logica(name, image, email);
     }
   }, [isAuthenticated, user]);
-  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-  const login = () => {
-    Swal.fire({
-      title: 'Tienes cuenta en PetsHouse?',
-      showDenyButton: true,
-      showCancelButton: true,
-      confirmButtonText: 'Login',
-      denyButtonText: `Ingresa con gmail`
-    }).then((result) => {
-      if (result.isConfirmed) {
-        navigate('/loginAPI');
-      } else if (result.isDenied) {
-        loginWithRedirect();
-      }
-    });
-  };
   // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
   async function auth0Logica(
     name: string | undefined,
@@ -54,17 +34,12 @@ const Home: React.FC = () => {
           localStorage.setItem('name', res.data.name);
           localStorage.setItem('image', res.data.image);
           localStorage.setItem('rol', res.data.rol);
+          getLogged(true);
         });
     } catch (error) {
       console.log(error);
     }
   }
-  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-  const logoutApp = () => {
-    getLogged(false);
-    localStorage.clear();
-    logout({ logoutParams: { returnTo: window.location.origin } });
-  };
 
   return (
     <div>
@@ -78,12 +53,6 @@ const Home: React.FC = () => {
           <Paginado />
         </div>
       </div>
-      <button onClick={logoutApp}>Logout</button>
-      {logged ? (
-        <button onClick={logoutApp}>Logout</button>
-      ) : (
-        <button onClick={login}>Login</button>
-      )}
     </div>
   );
 };
---
+++ src/Componentes/UserDetail/PublicacionesUsuario.tsx
@@ -0,0 +1,214 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
+import {
+  Button,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TextField,
+  Box,
+  Dialog,
+  Select,
+  MenuItem,
+  type SelectChangeEvent,
+  FormControl,
+  InputLabel
+} from '@mui/material';
+import axios from 'axios';
+import { traerPets, getUserPublications } from 'helpers';
+import { useCustomSelector } from 'hooks/redux';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { type Pet } from 'redux/slices/mascotas';
+import './dashboardUser.css';
+
+const PetDash: React.FC = () => {
+  useEffect((): void => {
+    const idUser = Number(localStorage.getItem('id'));
+    traerPets();
+    getUserPublications(idUser);
+  }, []);
+  const pets = useCustomSelector((state) => state.pets.allPets);
+  const [petInfo, setPetInfo] = useState<Pet>({
+    id: -1,
+    name: '',
+    image: '',
+    age: 0,
+    description: '',
+    size: '',
+    healthBook: false,
+    animal: '',
+    active: false,
+    province: '',
+    location: '',
+    sex: '',
+    createdAt: '',
+    UserId: 0
+  });
+  const [open, setOpen] = useState(false);
+
+  function editHandler(e: React.MouseEvent<HTMLButtonElement>): void {
+    const button: HTMLButtonElement = e.currentTarget;
+    const currentPet: Pet = pets.filter(
+      (pet) => pet.id === Number(button.value)
+    )[0];
+    setPetInfo(currentPet);
+    setOpen(true);
+  }
+
+  function onClose(): void {
+    setOpen(false);
+  }
+
+  function changeHandler(e: React.ChangeEvent<HTMLInputElement>): void {
+    const property = e.target.name;
+    // eslint-disable-next-line no-useless-computed-key
+    setPetInfo({ ...petInfo, [property]: e.target.value });
+  }
+
+  const selectHandler = (e: SelectChangeEvent): void => {
+    let activo = false;
+    const property = e.target.name;
+    if (e.target.value === 'true') activo = true;
+    setPetInfo({ ...petInfo, [property]: activo });
+  };
+
+  async function submitEdit(): Promise<any> {
+    try {
+      const toSend = { ...petInfo, ...{ idPet: petInfo.id } };
+      console.log('pet info: ', toSend);
+      await axios.put('/pets', toSend);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const modalWindow = (
+    <Box p={2}>
+      <div className="editContainer">
+        <TextField
+          fullWidth
+          name="name"
+          onChange={changeHandler}
+          value={petInfo.name}
+          label="Nombre"
+        />
+        <TextField
+          fullWidth
+          name="age"
+          type="number"
+          onChange={changeHandler}
+          value={petInfo.age}
+          label="Edad"
+        />
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Activo</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={String(petInfo.active)}
+            label="Estado"
+            onChange={selectHandler}
+            name="active"
+          >
+            <MenuItem value="true">Si</MenuItem>
+            <MenuItem value="false">No</MenuItem>
+          </Select>
+        </FormControl>
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">
+            Libreta Sanitaria
+          </InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={String(petInfo.healthBook)}
+            label="Libreta Sanitaria"
+            onChange={selectHandler}
+            name="healthBook"
+          >
+            <MenuItem value="true">Disponible</MenuItem>
+            <MenuItem value="false">No Disponible</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+      <Button color="error" onClick={onClose}>
+        Cerrar Ventana
+      </Button>
+      <Button onClick={submitEdit}>Enviar</Button>
+    </Box>
+  );
+
+  return (
+    <div className="showContainer">
+      <TableContainer className="tableContainer">
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell align="center" colSpan={6}>
+                <h2>Publicaciones</h2>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Nombre</TableCell>
+              <TableCell align="center">Imagen</TableCell>
+              <TableCell align="center">Activo</TableCell>
+              <TableCell align="center">Provincia</TableCell>
+              <TableCell align="center">Fecha de Publicaion</TableCell>
+              <TableCell align="left">
+                <Link to={'/publicar'}>
+                  <Button color="primary" size="small" variant="contained">
+                    Añadir Mascota
+                  </Button>
+                </Link>
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {pets.map((pet, index) => {
+              return (
+                <TableRow key={index}>
+                  <TableCell>
+                    <img src={pet.image} alt="No tiene imagen" />
+                  </TableCell>
+                  <TableCell>{pet.name}</TableCell>
+                  <TableCell align="center">
+                    {pet.active && <>Si</>}
+                    {!pet.active && <>No</>}
+                  </TableCell>
+                  <TableCell align="center">{pet.province}</TableCell>
+                  <TableCell align="center">{pet.createdAt}</TableCell>
+                  <TableCell align="center">
+                    <Button
+                      size="small"
+                      value={pet.id}
+                      variant="outlined"
+                      onClick={editHandler}
+                    >
+                      Editar
+                    </Button>
+                  </TableCell>
+                  {/* <TableCell>
+                    <Button
+                      size="small"
+                      value={pet.id}
+                      color="error"
+                      variant="outlined"
+                    >
+                      Eliminar
+                    </Button>
+                  </TableCell> */}
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <Dialog open={open}>{modalWindow}</Dialog>
+    </div>
+  );
+};
+
+export default PetDash;
