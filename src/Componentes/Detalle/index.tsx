fe6deed2b195c7bf3d24d86b21990f4838c4e843
Kevin <kevinesteban2010@hotmail.com>
2023-03-09 03:59:33 -0500

    detalle actualizado

--- src/Componentes/Detalle/index.tsx
+++ src/Componentes/Detalle/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useCustomSelector } from 'hooks/redux';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
@@ -14,7 +15,7 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { Button, CardActionArea, CardActions } from '@mui/material';
+import { Box, Button, CardActionArea, CardActions } from '@mui/material';
 import { styled } from '@mui/material/styles';
 
 const ButtonAca = styled(Button)({
@@ -30,6 +31,7 @@ const Detalle: React.FC = (): any => {
   const [open2, setOpen2] = useState(false);
   const pet = useCustomSelector((state) => state.pets.petDetalle);
   const navigate = useNavigate();
+  const idUser = Number(localStorage.getItem('id'));
 
   const handleClickOpen = (): void => {
     setOpen(true);
@@ -57,10 +59,74 @@ const Detalle: React.FC = (): any => {
     setOpen(false);
   };
 
+  const modalWindow1 = (
+    <Box>
+      <DialogTitle id="alert-dialog-title">
+        {'Esta seguro/a de que desea adoptar esta mascota?'}
+      </DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-description">
+          Si es asi, le enviaremos un email con toda la informacion necesaria
+          para que pueda contactarse con la persona a cargo de la misma.
+        </DialogContentText>
+        <DialogContentText variant="subtitle2">
+          {'(Debe estar Logueado para continuar)'}
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button disabled={idUser < 1 && true} onClick={contacto}>
+          Contactar
+        </Button>
+        <Button onClick={handleClose}>Cancelar</Button>
+      </DialogActions>
+    </Box>
+  );
+
+  const modalWindow2 = (
+    <Box>
+      <DialogTitle id="alert-dialog-title">
+        {'Muchas gracias por iniciar el proceso de adopcion!'}
+      </DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-description">
+          A la brevedad nos contactaremos contigo por email.
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={inicio}>Volver al inicio</Button>
+      </DialogActions>
+    </Box>
+  );
   return (
     <>
       {/* <div>CARD DE MATERIAL</div> */}
-      <Card className="over" sx={{ maxWidth: 345 }}>
+      <div className="detailContainer">
+        <div className="textDetailContainer">
+          <Typography variant="h3">{pet.Pet.name}</Typography>
+          <Typography variant="h5">Animal: {pet.Pet.animal}</Typography>
+          <Typography variant="h5">Edad: {pet.Pet.age}</Typography>
+          <div id="descriptionText">
+            <Typography variant="h5" fontSize={35}>
+              Descripción:
+            </Typography>
+            <Typography variant="h5" fontSize={20}>
+              {pet.Pet.description}
+            </Typography>
+          </div>
+          <Typography variant="h5">Dueño Actual: {pet.User.name}</Typography>
+          <Button
+            variant="contained"
+            style={{ backgroundColor: '#e05f64' }}
+            onClick={handleClickOpen}
+          >
+            Contactarse
+          </Button>
+        </div>
+        <div className="divImagenDetail">
+          <img id="detailImg" src={pet.Pet.image} alt="petDetailImage" />
+        </div>
+      </div>
+      {/* <Card className="over" sx={{ maxWidth: 345 }}>
         <CardActionArea>
           <CardMedia
             component="img"
@@ -114,47 +180,25 @@ const Detalle: React.FC = (): any => {
           <ButtonAca size="small" color="primary" onClick={handleClickOpen}>
             Contactarse
           </ButtonAca>
-        </CardActions>
-        <Dialog
-          open={open}
-          onClose={handleClose}
-          aria-labelledby="alert-dialog-title"
-          aria-describedby="alert-dialog-description"
-        >
-          <DialogTitle id="alert-dialog-title">
-            {'Esta seguro/a de que desea adoptar esta mascota?'}
-          </DialogTitle>
-          <DialogContent>
-            <DialogContentText id="alert-dialog-description">
-              Si es asi, le enviaremos un email con toda la informacion
-              necesaria para que pueda contactarse con la persona a cargo de la
-              misma.
-            </DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={contacto}>Contactar</Button>
-            <Button onClick={handleClose}>Cancelar</Button>
-          </DialogActions>
-        </Dialog>
-        <Dialog
-          open={open2}
-          onClose={handleClose2}
-          aria-labelledby="alert-dialog-title"
-          aria-describedby="alert-dialog-description"
-        >
-          <DialogTitle id="alert-dialog-title">
-            {'Muchas gracias por iniciar el proceso de adopcion!'}
-          </DialogTitle>
-          <DialogContent>
-            <DialogContentText id="alert-dialog-description">
-              A la brevedad nos contactaremos contigo por email.
-            </DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={inicio}>Volver al inicio</Button>
-          </DialogActions>
-        </Dialog>
-      </Card>
+        </CardActions> 
+      </Card> */}
+      {/*          Dialogs             */}
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        {modalWindow1}
+      </Dialog>
+      <Dialog
+        open={open2}
+        onClose={handleClose2}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        {modalWindow2}
+      </Dialog>
     </>
   );
 };
